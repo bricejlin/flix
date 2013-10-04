d30cd16d450a1369714f815df0d67cc993f5e058
bricejlin <bricejlin@gmail.com>
2013-10-04 14:33:16 -0400

    ready to deploy v 1.0

---
+++ spec/features/create_review_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe "Creating a new review" do 
+  let(:movie) { Movie.create(movie_attributes) }
+
+  it "saves the review" do    
+    visit movie_url(movie)
+    
+    click_link 'Write Review'
+    
+    expect(current_path).to eq(new_movie_review_path(movie))
+    
+    fill_in "Name", with: "Roger Ebert"
+    choose "review_stars_3"
+    fill_in "Comment", with: "I laughed, I cried, I spilled my popcorn!"
+            
+    click_button 'Post Review'
+  
+    expect(current_path).to eq(movie_reviews_path(movie))
+    
+    expect(page).to have_text("Thanks for your review!")
+  end
+  
+  it "does not save the review if it's invalid" do    
+    visit new_movie_review_url(movie)
+    
+    expect { 
+      click_button 'Post Review' 
+    }.not_to change(Review, :count)
+        
+    expect(page).to have_text('error')
+  end
+end
